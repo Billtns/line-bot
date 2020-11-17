496f05bc80b0e090d68014ca7c38c984cc7d8d40
Bill2543 <70052266+Bill2543@users.noreply.github.com>
2020-11-17 15:14:51 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -18,11 +18,11 @@ foreach ($request_json['events'] as $event)
 			//$reply_message = mySQL_selectAll('http://s61160179.kantit.com/json_select.php');
 			
 				
-				$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
+				//$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
 				
 				
 				
-				//$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php?sid='.$text);	
+				$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php?sid='.$text);	
 			
 			
 		} else {
